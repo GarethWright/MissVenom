4454fe2bbd5947c674fcde7a5dc124ae57650882
Max Kovaljov <shirioko@gmail.com>
2013-04-24 00:04:21 +0200

    Initial commit
    I've included the WebServer library into the project and also included
    the certificate as an embedded resource, so you can simply download the
    .exe and run it without worrying about DLLs and other files.

---
+++ MissVenom/Form1.cs
@@ -0,0 +1,84 @@
+﻿using HttpServer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private byte[] getCertificate()
+        {
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(GetType(), @"server.pfx");
+            var bytes = new byte[stream.Length];
+            stream.Read(bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            var certificate = new X509Certificate2(this.getCertificate(), "banana");
+            //certificate.PrivateKey = getPrivateKey();
+            var listener = (SecureHttpListener)HttpServer.HttpListener.Create(IPAddress.Any, 443, certificate);
+            listener.UseClientCertificate = true;
+            listener.RequestReceived += OnRequest;
+            listener.Start(5);
+            this.AddListItem("Listening...");
+            this.AddListItem(" ");
+        }
+
+        delegate void AddListItemCallback(String text);
+
+        private void AddListItem(String data)
+        {
+            if (this.textBox1.InvokeRequired)
+            {
+                AddListItemCallback a = new AddListItemCallback(AddListItem);
+                this.Invoke(a, new object[] { data });
+            }
+            else
+            {
+                this.textBox1.AppendText(data + "\r\n");
+                this.textBox1.DeselectAll();
+            }
+        }
+
+        private void OnRequest(object sender, RequestEventArgs e)
+        {
+            String url = "https://v.whatsapp.net" + e.Request.Uri.PathAndQuery;
+            HttpWebRequest request = HttpWebRequest.Create(url) as HttpWebRequest;
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            e.Response.ContentType.Value = response.ContentType;
+            e.Response.ContentLength.Value = response.ContentLength;
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader responseReader = new StreamReader(responseStream);
+            String data = responseReader.ReadToEnd();
+
+            byte[] data2 = Encoding.Default.GetBytes(data);
+
+            e.Response.Body.Write(data2, 0, data2.Length);
+
+            this.AddListItem("REQUEST:  " + e.Request.Uri.AbsoluteUri);
+            this.AddListItem("RESPONSE: " + data);
+            this.AddListItem(" ");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
